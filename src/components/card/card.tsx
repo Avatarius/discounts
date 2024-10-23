f58c3cd696a226ac64f641036f8af409b111d536
Chingis Choybsonov <avatarius93@gmail.com>
2024-10-24 01:07:20 +0300

    4 карточка

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -12,38 +12,35 @@ function Card(props: ICardProps) {
   const [disountData, noDiscountData, biggerDiscountData] = data;
   const cardClassList = clsx(
     "flex",
-    "items-center",
     "rounded-card",
     "border-card",
-    isHorizontal ? "col-span-3 mt-[36px] p-card-horizontal" : "flex-col p-card"
-  );
-  const priceClassList = clsx(
-    "flex",
-    "flex-col",
-    isHorizontal ? "m-card-price-horizontal" : "m-card-price"
-  );
-  const discountClassList = clsx(
-    "font-['pt-root-ui']",
-
-    "tracking-[-2px]",
-    isHorizontal ?
-    "m-card-discount text-card-discount-horizontal" :
-    'text-card-discount'
-  );
-  const noDiscountClassList = clsx(
-    "text-card-no-discount",
-    "self-end",
-    !isHorizontal && "m-card-no-discount-horizontal"
+    isHorizontal ? "col-span-3 mt-[36px] p-card-horizontal items-start" : "flex-col p-card items-center"
   );
   const titleClassList = clsx(
     "font-BebasNeueCyrillic",
     isHorizontal
       ? "text-card-title-horizontal m-card-title-horizontal"
-      : "text-card-title m-card-title"
+      : "text-card-title self-start mb-card-title"
+  );
+  const priceClassList = clsx(
+    "flex",
+    "flex-col",
+    isHorizontal ? "mr-card-price-horizontal" : "mb-card-price"
+  );
+  const discountClassList = clsx(
+    "font-['pt-root-ui']",
+    "tracking-[-2px]",
+    isHorizontal ?
+    "text-card-discount-horizontal" :
+    'text-card-discount'
+  );
+  const noDiscountClassList = clsx(
+    "text-card-no-discount",
+    "self-end",
   );
   const descriptionClassList = clsx(
     "text-card-description",
-    isHorizontal ? 'mt-card-description-horizontal text-left' : "max-w-[120px] text-center"
+    isHorizontal ? 'mt-card-description-horizontal text-left max-w-[165px]' : "max-w-[120px] text-center"
   );
   return (
     <article className={cardClassList}>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,63 +4,66 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'pt-root-ui': ['"PT Root UI", sans-serif'],
-        'BebasNeue': ['"BebasNeue", sans-serif'],
-        'BebasNeueCyrillic': ['BebasNeueCyrillic, sans-serif'],
-        'rubik': ['Rubik, sans-serif']
+        "pt-root-ui": ['"PT Root UI", sans-serif'],
+        BebasNeue: ['"BebasNeue", sans-serif'],
+        BebasNeueCyrillic: ["BebasNeueCyrillic, sans-serif"],
+        rubik: ["Rubik, sans-serif"],
       },
       fontSize: {
-        'heading': ['40px', {fontWeight: 700}],
-        'base': ['30px', {fontWeight: 700}],
-        'timer-digits': ['60px', {fontWeight: 400, lineHeight: 1}],
-        'timer-letters': ['16px', {fontWeight: 700}],
-        'card-title': ['30px', {fontWeight: 400, lineHeight: 1}],
-        'card-title-horizontal': ['38px', {fontWeight: 400, lineHeight: 1}],
-        'card-discount': ['50px', {fontWeight: 700, lineHeight: 1.1, letterSpacing: '-0.04em'}],
-        'card-discount-horizontal': ['50px', {fontWeight: 700, lineHeight: 1.0, letterSpacing: '-0.04em'}],
-        'card-no-discount': ['24px', {fontWeight: 500, lineHeight: 1.1}],
-        'card-description': ['16px', {fontWeight: 500, lineHeight: 1.3}]
+        heading: ["40px", { fontWeight: 700 }],
+        base: ["30px", { fontWeight: 700 }],
+        "timer-digits": ["60px", { fontWeight: 400, lineHeight: 1 }],
+        "timer-letters": ["16px", { fontWeight: 700 }],
+        "card-title": ["30px", { fontWeight: 400, lineHeight: 1 }],
+        "card-title-horizontal": ["38px", { fontWeight: 400, lineHeight: 1 }],
+        "card-discount": [
+          "50px",
+          { fontWeight: 700, lineHeight: 1, letterSpacing: "-0.04em" },
+        ],
+        "card-discount-horizontal": [
+          "50px",
+          { fontWeight: 700, lineHeight: 1.0, letterSpacing: "-0.04em" },
+        ],
+        "card-no-discount": ["24px", { fontWeight: 500, lineHeight: 1 }],
+        "card-description": ["16px", { fontWeight: 500, lineHeight: 1.3 }],
       },
       colors: {
-        'background': '#f5f7f7',
-        'timer-digits': '#01b9c5',
-        'timer-letters': '#818798',
-        'card-title': '#687078',
-        'card-title-horizontal': '#2d3242',
-        'card-discount': '#2d3242',
-        'card-no-discount': '#95979f',
-        'card-description': '#2f4353'
+        background: "#f5f7f7",
+        "timer-digits": "#01b9c5",
+        "timer-letters": "#818798",
+        "card-title": "#687078",
+        "card-title-horizontal": "#2d3242",
+        "card-discount": "#2d3242",
+        "card-no-discount": "#95979f",
+        "card-description": "#2f4353",
       },
       padding: {
-        'header': '8px 69px 14px 0',
-        'card': '45px 0 26px 0',
-        'card-horizontal': '0 0 21px 19px',
-        'card-title': '32px',
-        'card-discount': '6px',
-        'card-no-discount': '0px'
+        header: "8px 69px 14px 0",
+        card: "43px 29px 26px",
+        "card-horizontal": "26px 0 21px 28px",
+        "card-title": "32px",
+        "card-discount": "6px",
       },
       margin: {
-        'card-title': '0px 31px 20px 0px',
-        'card-title-horizontal': '10px',
-        'card-price': '0 0 18px 0',
-        'card-price-horizontal': '25px 57px 0 10px',
-        'card-discount': '0px',
-        'card-description-horizontal': '32px'
+        "card-title": "25px",
+        "card-title-horizontal": "10px 20px 0 0",
+        "card-price": "21px",
+        "card-price-horizontal": "59px",
+        "card-description-horizontal": "11px",
       },
       inset: {
-        'timer': 'calc(100% - 6px) 0'
+        timer: "calc(100% - 6px) 0",
       },
       borderRadius: {
-        'card': '20px'
+        card: "20px",
       },
       borderWidth: {
-        'card': '2px',
+        card: "2px",
       },
       borderColor: {
-        'card': '#01b9c5'
-      }
+        card: "#01b9c5",
+      },
     },
   },
   plugins: [],
-}
-
+};
